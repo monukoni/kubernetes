484198f40724a6af200afa38bb0652833c9d1795
monukoni <kartoplya213@gmail.com>
2026-01-28 14:50:37 +0200

    test

--- terraform/layers/app/data.tf
+++ terraform/layers/app/data.tf
@@ -1,34 +1,34 @@
-data "aws_eks_cluster_auth" "eks" {
-  name = data.terraform_remote_state.eks.outputs.cluster_name
-}
+# data "aws_eks_cluster_auth" "eks" {
+#   name = data.terraform_remote_state.eks.outputs.cluster_name
+# }
 
-data "aws_eks_cluster" "eks" {
-  name = data.terraform_remote_state.eks.outputs.cluster_name
-}
-
-
-data "terraform_remote_state" "eks" {
-  backend = "s3"
-  config = {
-    bucket = "terraform-project-state-bucket312"
-    key    = "terraform-ops.tfstate"
-    region = var.region
-  }
-  workspace = var.name
-}
-
-data "aws_resourcegroupstaggingapi_resources" "consul_elb_search" {
-  resource_type_filters = ["elasticloadbalancing:loadbalancer"]
-
-  tag_filter {
-    key    = "kubernetes.io/service-name"
-    values = ["consul/consul-ingress-gateway"]
-  }
-
-  depends_on = [helm_release.load-balancer]
-}
+# data "aws_eks_cluster" "eks" {
+#   name = data.terraform_remote_state.eks.outputs.cluster_name
+# }
 
 
-data "aws_elb" "consul_ingress" {
-  name = local.elb_name
-}
+# data "terraform_remote_state" "eks" {
+#   backend = "s3"
+#   config = {
+#     bucket = "terraform-project-state-bucket312"
+#     key    = "terraform-ops.tfstate"
+#     region = var.region
+#   }
+#   workspace = var.name
+# }
+
+# data "aws_resourcegroupstaggingapi_resources" "consul_elb_search" {
+#   resource_type_filters = ["elasticloadbalancing:loadbalancer"]
+
+#   tag_filter {
+#     key    = "kubernetes.io/service-name"
+#     values = ["consul/consul-ingress-gateway"]
+#   }
+
+#   depends_on = [helm_release.load-balancer]
+# }
+
+
+# data "aws_elb" "consul_ingress" {
+#   name = local.elb_name
+# }
--- terraform/layers/app/providers.tf
+++ terraform/layers/app/providers.tf
@@ -38,6 +38,6 @@ provider "helm" {
   }
 }
 
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
+# provider "cloudflare" {
+#   api_token = var.cloudflare_api_token
+# }
