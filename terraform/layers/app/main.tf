d5da37c43774697d893d7926519029199d0563d1
monukoni <kartoplya213@gmail.com>
2026-01-28 14:31:27 +0200

    added app of apps bootstrap config

--- terraform/layers/app/main.tf
+++ terraform/layers/app/main.tf
@@ -1,17 +1,28 @@
-resource "helm_release" "load-balancer" {
-  name  = "load-balancer"
-  chart = "../../../helm/load-balancer"
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  create_namespace = true
+  namespace        = "argocd"
+
+  version = "9.3.7"
 
   wait            = true
   cleanup_on_fail = true
 }
 
-resource "cloudflare_dns_record" "main_dns_record" {
-  zone_id = var.zone_id
-  name    = "@"
-  ttl     = 300
-  type    = "CNAME"
-  comment = "record to aws lb"
-  content = data.aws_elb.consul_ingress.dns_name
-  proxied = false
+resource "kubernetes_manifest" "app_of_apps" {
+  manifest = file("../../../argocd/app-of-apps.yaml")
+  depends_on = [helm_release.argocd]
 }
+
+# resource "cloudflare_dns_record" "main_dns_record" {
+#   zone_id = var.zone_id
+#   name    = "@"
+#   ttl     = 300
+#   type    = "CNAME"
+#   comment = "record to aws lb"
+#   content = data.aws_elb.consul_ingress.dns_name
+#   proxied = false
+# }
