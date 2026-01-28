960268a9a1d4d152c30c50d5b5463c539ee50a5b
monukoni <kartoplya213@gmail.com>
2026-01-28 14:53:52 +0200

    test

--- terraform/layers/app/data.tf
+++ terraform/layers/app/data.tf
@@ -1,10 +1,10 @@
-# data "aws_eks_cluster_auth" "eks" {
-#   name = data.terraform_remote_state.eks.outputs.cluster_name
-# }
+data "aws_eks_cluster_auth" "eks" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
 
-# data "aws_eks_cluster" "eks" {
-#   name = data.terraform_remote_state.eks.outputs.cluster_name
-# }
+data "aws_eks_cluster" "eks" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
 
 
 # data "terraform_remote_state" "eks" {
